ba913d86a491358d5d2cb2d5d5308ff5043c6dc5
Tom Mount <tmountjr@gmail.com>
2025-10-27 16:25:20 -0400

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import fs from "fs";
+import * as mmdb from "mmdb-lib";
+import express, { NextFunction } from "express";
+
+const dbFilename = "../databases/standard_location.mmdb";
+
+const db = fs.readFileSync(dbFilename);
+const mmdbReader = new mmdb.Reader(db);
+
+const ipinfoMiddleware = (req: any, _: any, next: NextFunction) => {
+  const ip = req.ip;
+  let ipinfo = {}
+  if (ip) {
+    ipinfo = Object.assign({}, mmdbReader.get(ip))
+  }
+  req.ipinfo = ipinfo;
+  next();
+};
+
+const app = express();
+const port = 3000;
+
+app.use(ipinfoMiddleware);
+app.enable("trust proxy");
+
+app.get("/", (req: any, res: any) => {
+  res.json(req.ipinfo)
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
